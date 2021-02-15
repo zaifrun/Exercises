634092a8172ff109de890389d5ecc6749b4b2520
zaifrun <martinknud@gmail.com>
2021-02-15 11:57:12 +0100

    updated the pensionist class with comments

--- app/src/main/java/org/pondar/exercises/Pensionist.kt
+++ app/src/main/java/org/pondar/exercises/Pensionist.kt
@@ -1,5 +1,6 @@
 package org.pondar.exercises
 
+//Notice the very important use of "var"
 class Pensionist (name:String,age: Int, var retirementYear:Int) : Person(name,age) {
 
     override fun toString(): String {
